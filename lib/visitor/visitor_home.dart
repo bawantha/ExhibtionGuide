0f4a26e0326b625fd025a1b7cf1b9c8691dfd7ed
bawantha <bawwa.16@cse.mrt.ac.lk>
2020-07-21 12:55:47 +0530

    Day 5

--- lib/visitor/visitor_home.dart
+++ lib/visitor/visitor_home.dart
@@ -4,7 +4,6 @@ import 'package:connectivity/connectivity.dart';
 import 'package:exhibition_guide/services/db_service.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
 
 class VisitorHome extends StatefulWidget {
   @override
@@ -24,6 +23,13 @@ class _VisitorHomeState extends State<VisitorHome> {
     return await Connectivity().getWifiBSSID();
   }
 
+  Stream<String> getStremWIFIBSD() async* {
+    while (true) {
+      String result = await Connectivity().getWifiBSSID();
+      yield result;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,85 +45,95 @@ class _VisitorHomeState extends State<VisitorHome> {
             (BuildContext context, AsyncSnapshot<ConnectivityResult> snapshot) {
           if (snapshot.hasData) {
             if (snapshot.data == ConnectivityResult.wifi) {
-              bool _isPermissionGranted = Provider.of<bool>(context)??false;
-              String mac=Provider.of<String>(context);
-              print(mac);
-              if (_isPermissionGranted) {
-                return Stack(
-                  children: <Widget>[
-                    Positioned(
-                      top: 50,
-                      left: 0,
-                      child: Container(
-                        width: 150,
-                        height: 250,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                        ),
-                        child: Center(
-                          child: RaisedButton(
-                            textColor: Colors.white,
-                            color: mac == "90:61:0c:52:b5:2f"
-                                ? Colors.green
-                                : Colors.grey,
-                            child: Text("A"),
-                            onPressed: () async {
-//                              await _db.visitorVisited("90:61:0c:52:b5:2f");
-//                                          Navigator.pushNamed(
-//                                              context, '/stall');
-                            },
-                            shape: CircleBorder(),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 150,
-                      right: 0,
-                      child: Container(
-                        width: 150,
-                        height: 250,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                        ),
-                        child: Center(
-                          child: RaisedButton(
-                            textColor: Colors.white,
-                            color: mac == "7c:76:68:80:70:80"
-                                ? Colors.green
-                                : Colors.grey,
-                            child: Text("B"),
-                            onPressed: () {},
-                            shape: CircleBorder(),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      child: Container(
-                        width: 300,
-                        height: 150,
-                        child: Center(
-                          child: RaisedButton(
-                            textColor: Colors.white,
-                            color: Colors.grey,
-                            child: Text("C"),
-                            onPressed: () {},
-                            shape: CircleBorder(),
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              } else {
-                return Text("No permisison");
-              }
+              return FutureBuilder<bool>(
+                builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                  if (snapshot.data) {
+                    return StreamBuilder<String>(
+                      builder: (BuildContext context,
+                          AsyncSnapshot<String> macdata) {
+                        if (macdata.hasData) {
+                          return Stack(
+                            children: <Widget>[
+                              Positioned(
+                                top: 50,
+                                left: 0,
+                                child: Container(
+                                  width: 150,
+                                  height: 250,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                  ),
+                                  child: Center(
+                                    child: RaisedButton(
+                                      textColor: Colors.white,
+                                      color: macdata.data == "7c:76:68:80:70:80"
+                                          ? Colors.green
+                                          : Colors.grey,
+                                      child: Text("A"),
+                                      onPressed: () {
+                                        Navigator.pushNamed(context, '/stall');
+                                      },
+                                      shape: CircleBorder(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                top: 150,
+                                right: 0,
+                                child: Container(
+                                  width: 150,
+                                  height: 250,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                  ),
+                                  child: Center(
+                                    child: RaisedButton(
+                                      textColor: Colors.white,
+                                      color: macdata.data == "90:61:0c:52:b5:2f"
+                                          ? Colors.green
+                                          : Colors.grey,
+                                      child: Text("B"),
+                                      onPressed: () {},
+                                      shape: CircleBorder(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                child: Container(
+                                  width: 300,
+                                  height: 150,
+                                  child: Center(
+                                    child: RaisedButton(
+                                      textColor: Colors.white,
+                                      color: Colors.grey,
+                                      child: Text("C"),
+                                      onPressed: () {},
+                                      shape: CircleBorder(),
+                                    ),
+                                  ),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        } else {
+                          return Text("no data");
+                        }
+                      },
+                      stream: getStremWIFIBSD(),
+                    );
+                  } else {
+                    return Text("NO PERMISSION ALLOWD");
+                  }
+                },
+                future: Permission.locationAlways.isGranted,
+              );
             } else {
               return Text("Connect Free WIFI");
             }
